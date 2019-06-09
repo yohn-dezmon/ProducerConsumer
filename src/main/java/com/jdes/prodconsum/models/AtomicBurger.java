bf02ffe89a9b8e5568117f037129a1d8a0c94996
Johnboi <johndesmond631@gmail.com>
2019-06-09 16:57:46 -0500

    comments

--- src/main/java/com/jdes/prodconsum/models/AtomicBurger.java
+++ src/main/java/com/jdes/prodconsum/models/AtomicBurger.java
@@ -6,6 +6,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AtomicBurger {
 	
+	// making the class variables atomic means that once one of the threads
+	// has access to the Atomic variable, the method involving that variable
+	// must be complete before another thread can access it.
+	// Atomic variables differ from synchronized variables because the synchronization
+	// is achieved through compare-and-swap as opposed to locking
 	private AtomicBoolean cooked = new AtomicBoolean(false);
 	private AtomicBoolean delivered = new AtomicBoolean(false);
 	private AtomicBoolean inBuffer = new AtomicBoolean(false);
